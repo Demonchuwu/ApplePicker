6b6af66d68c485166c74dea4a2f5994e34dc3d80
Demonchuwu <cnm3575@utulsa.edu>
2022-02-02 15:58:34 -0600

    Updated Code
    -Updated ApplePicker code
    +Added the summoning of the basket to the bottom of the screen
    -Updated Basket code
    +Added movement script to basket
    +Added destroying the apples when in contact

--- ApplePicker-Unity/Assets/Scripts/ApplePicker.cs
+++ ApplePicker-Unity/Assets/Scripts/ApplePicker.cs
@@ -1,18 +1,41 @@
+/***
+* Created by Cristian Misla
+* Created by: Cristian Misla
+* Date Created: 1/31/2022
+*
+* Last Edited By Cristian Misla
+* Last Edited: 2/2/2022
+* 
+* Description: Game Manager for ApplePicker
+***/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ApplePicker : MonoBehaviour
 {
+    [Header("SET IN INSPECTOR")]
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+       for(int i=0; i<numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+        }//end of for()
+    }//end of Start()
 
     // Update is called once per frame
     void Update()
     {
         
-    }
+    }//end of Update()
 }
--- ApplePicker-Unity/Assets/Scripts/Basket.cs
+++ ApplePicker-Unity/Assets/Scripts/Basket.cs
@@ -1,3 +1,13 @@
+/***
+* Created by Cristian Misla
+* Created by: Cristian Misla
+* Date Created: 1/31/2022
+*
+* Last Edited By Cristian Misla
+* Last Edited: 2/2/2022
+* 
+* Description: Movement script for the Basket
+***/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +23,19 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }//end of Update()
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if(collidedWith.tag == "Apple")
+            {
+            Destroy(collidedWith);
+        }//end of if()
+    }//end of OnCollisionEnter()
 }
